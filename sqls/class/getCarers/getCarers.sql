e335afddf847e8c8c50e7bb406b9fbe177c3220e
이재현 <jaehyunlee@ijaehyeon-ui-MacBookPro.local>
2021-12-17 00:16:48 +0900

    renew

--- sqls/class/getCarers/getCarers.sql
+++ sqls/class/getCarers/getCarers.sql
@@ -1,8 +1,14 @@
-select
-    *
-from
-    members m
-    left join school_roles sr on sr.id = m.school_role_id
+select 
+	m.id member_id,
+	m.nickname member_name,
+	m.image_id member_image_id,
+	c.id class_id, 
+	c.name class_name,
+	sr.grade member_grade
+from 
+	members m
+	left join school_roles sr on sr.id = m.school_role_id
+	left join class c on c.id = m.class_id
 where
     m.school_id = '${schoolId}'
     and m.class_id = '${classId}'
