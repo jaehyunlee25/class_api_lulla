942bcecac8a14b2dbf73ec12c5d7967daead4888
jaehyunlee <jaehyunlee25@gmail.com>
2021-12-17 22:30:11 +0900

    renew

---
+++ sqls/class/getKids/getKidsClassNoSearch.sql
@@ -0,0 +1,17 @@
+select 
+	k.id kid_id,
+	m.class_id class_id,
+	c.name class_name,
+	k.name kid_name,
+	k.image_id kid_image_id	
+from 
+	members m
+	left join school_roles sr on sr.id = m.school_role_id
+	left join class c on c.id = m.class_id
+	left join kid k on k.id = m.kid_id
+where 
+	m.school_id = '${schoolId}'
+	and sr.grade = 5
+	and m.class_id = '${classId}'
+order by
+	k.name asc;
